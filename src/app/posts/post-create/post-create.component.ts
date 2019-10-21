33b7692290bdf9f51730d70386a170fc8fdb53b3
Chavez Maldonado Cesar Francisco <cesar.chavez@consultant.volvo.com>
2019-10-21 12:55:18 +0200

    Angular app first version.

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,43 @@
+// import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+// import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredTitle = '';
+  enteredContent = '';
+
+  // @Output()
+  // postCreatead = new EventEmitter<Post>();
+
+  constructor(public postsService: PostsService) {}
+
+  // onAddPost(form: NgForm) {
+  //   // const post: Post = {
+  //   //   title: this.enteredTitle,
+  //   //   content: this.enteredContent
+  //   // };
+  //   if (form.invalid) {
+  //     return;
+  //   }
+  //   const post: Post = {
+  //     title: form.value.title,
+  //     content: form.value.content
+  //   };
+  //   this.postCreatead.emit(post);
+  // }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.reset();
+  }
+}
