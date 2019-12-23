d16650ff2ee26414ac22be1f0074a62540a7c7cb
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2019-12-23 22:44:57 +0100

    Updating files.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
+const routes: RouterModule = [
+  { path: '', component: PostListComponent },
+  { path: '', component: PostListComponent }
+];
+
+@NgModule({
+
+})
+
+export class AppRoutingModule {}
+
